ec42d199bbf65f7436d5dd1e628b5e182f25b7cf
EE24BTECH11012 <bhavanisankargs@gmail.com>
2025-01-04 00:08:27 +0530

    write wtvr

--- Q1/codes/main.py
+++ Q1/codes/main.py
@@ -1,6 +1,7 @@
 #Calling C function in Python
 from ctypes import *
 import matplotlib.pyplot as plt
+import numpy as np
 
 #load the shared object file
 temp = CDLL('./func.so')
@@ -22,10 +23,18 @@ for elem in data :
     x_values.append(float(elem.split()[0]))
     y_values.append(float(elem.split()[1]))
 
-plt.plot(x_values, y_values, color='red', linestyle=':', label='$y = e^x$')
+x_func = [0.0]
+y_func = [2.0]
+h = 0.01
+for i in range(1000) :
+    x_func.append(x_func[i] + h)
+    y_func.append(y_func[i] + ((np.e)**x_func[i])*h)
+
+plt.plot(x_values, y_values, color='blue', linestyle='-', label='sim')
+plt.plot(x_func, y_func, color='green', linestyle='-.', label='$theory$')
 plt.xlabel('X-AXIS')
 plt.ylabel('Y-AXIS')
 plt.grid()
 plt.legend()
-plt.savefig('fig.png')
+#plt.savefig('fig.png')
 plt.show()
