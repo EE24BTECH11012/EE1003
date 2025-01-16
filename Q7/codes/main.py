c54af4a8e239eb688d6b3018a8b597656e535677
EE24BTECH11012 <bhavanisankargs@gmail.com>
2025-01-17 00:10:17 +0530

    commit

--- Q7/codes/main.py
+++ Q7/codes/main.py
@@ -2,9 +2,10 @@ import ctypes
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Load the shared object file
+# Load shared object files
 temp = ctypes.CDLL('./func.so')
 
+# Define function signatures for func.so
 temp.function.argtypes = [ctypes.c_double]
 temp.function.restype = ctypes.c_double
 
@@ -14,34 +15,34 @@ temp.derivative.restype = ctypes.c_double
 temp.NewtonRaphson.argtypes = [ctypes.c_double]
 temp.NewtonRaphson.restype = ctypes.c_double
 
-initGuess1 = float(input('Guess an initial value: '))
+# Newton-Raphson Method
+initGuess1 = float(input('Guess an initial value for root 1: '))
 root1 = temp.NewtonRaphson(initGuess1)
-print(root1)
+print(f"Root 1: {root1}")
 
-initGuess2 = float(input('Guess an initial value: '))
+initGuess2 = float(input('Guess an initial value for root 2: '))
 root2 = temp.NewtonRaphson(initGuess2)
-print(root2)
+print(f"Root 2: {root2}")
 
-x = [0]
-y = [2]
-j = 0
+# Plotting function
+x = []
+y = []
 i = 0
 h = 0.01
-while x[j] < 3 :
+while i <= 3:
     x.append(i)
     y.append(temp.function(i))
     i += h
-    j += 1
 
 plt.plot(x, y, color='red', label='$y = x^2 - 3x + 2$')
-plt.scatter(root1, temp.function(root1), label='Root1 at x=1')
-plt.scatter(root2, temp.function(root2), label='Root2 ar x=2')
-plt.axhline(0, color='black', linewidth=0.2)
-plt.axvline(0, color='black', linewidth=0.2)
+plt.scatter(root1, temp.function(root1), color='blue', label=f'Root 1 at x={root1:.2f}')
+plt.scatter(root2, temp.function(root2), color='green', label=f'Root 2 at x={root2:.2f}')
+plt.axhline(0, color='black', linewidth=0.5)
+plt.axvline(0, color='black', linewidth=0.5)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
 plt.legend()
-plt.xlabel('X-AXIS')
-plt.ylabel('Y-AXIS')
+plt.title("Newton-Raphson Root Finding")
 plt.savefig('../figs/fig.png')
 plt.show()
-
 
